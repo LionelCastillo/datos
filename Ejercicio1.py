dd216e3bbe894c9572d7334aa71678a2f29b7db8
LionelCastillo <105450241+LionelCastillo@users.noreply.github.com>
2022-07-09 13:22:34 -0300

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,21 @@
+nombre = 'julian'
+apellido = 'perez'
+edad = 25
+email = 'julianperez@gmail.com'
+telefono = 543795716534
+casado = True
+hijos = False
+amigos = ['matias', 'raul', 'esteban', 'francisco']
+peliculas = {'pelicula1': 'titanic',
+ 'pelicula2': 'los vengadores',
+ 'pelicula3': 'star wars',
+ 'pelicula4': 'avatar'}
+print('nombre',nombre)
+print('apellido', apellido)
+print('edad', edad)
+print('email', email)
+print('telefono', telefono)
+print('casado', casado)
+print('hijos', hijos)
+print('amigos', amigos)
+print('peliculas', peliculas)
---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+peso = float(input('ingrese su peso en kg: '))
+altura = float(input('ingrese su altura en mts: '))
+masa_corporal = float(peso/altura**2)
+print('Tu índice de masa corporal es: ', masa_corporal)
